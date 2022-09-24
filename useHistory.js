a2b003f651934d920849a7659f32f51fdbf77ade
nithishkk <109412506+nithishkk@users.noreply.github.com>
2022-09-24 06:31:28 -0400

    redirecting from one componet to another
    when we redirect from one component tp another componet in that case using useHistory() hook

---
+++ useHistory.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {useHistory} from 'react-router-dom';
+const Home =()=>{
+  const history= useHistory();
+  return(
+  <div>
+    <center>
+    <h1>welocme to home page</h1>
+    <button Onclick=()=> history.push('/dashboard)}>Login</button>
+                                      </center>
+                                      </div>
+  )
+  
+  
+  dashbored.js
+  
+  
+  
+  import recat from 'react'
+  
+  const dashborad=()=>{
+    return(
+      <div> <h1>weloc to dashboreda</div>
+      }
+  );
+  
